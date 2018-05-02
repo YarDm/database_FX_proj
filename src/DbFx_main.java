7ef5cebebe585caa1e0f169ff8226d9089a82d11
Ярослав <y.o.dmitriv@gmail.com>
2018-05-02 21:22:33 +0300

    first party changes

--- src/DbFx_main.java
+++ src/DbFx_main.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /*
@@ -9,10 +12,30 @@ public class DbFx_main extends Application {
 
     private Stage primaryStage;
 
+    public DbFx_main(){
+
+    }
+
     @Override
-    public void Start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception{
         this.primaryStage = primaryStage;
 
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/FrontView.fxml"));
+            Scene scene = new Scene(root);
+
+            this.primaryStage = primaryStage;
+            this.primaryStage.setTitle("Отчетное задание БД Java");
+            this.primaryStage.setScene(scene);
+            this.primaryStage.show();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        launch(args);
     }
 
 

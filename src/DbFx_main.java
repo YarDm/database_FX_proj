f3cf177798c0e29ccc9d4a164e2b50ba68a19db8
Ярослав <y.o.dmitriv@gmail.com>
2018-05-02 14:41:39 +0300

    Rewrite packages list

--- src/DbFx_main.java
+++ src/DbFx_main.java
@@ -1,2 +1,19 @@
-public class DbFx_main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/*
+*в JavaFX, который входит в стандартную комплектацию JDK,
+*стартовая точка приложения определяется методом старт и наслодования от Application
+*/
+public class DbFx_main extends Application {
+
+    private Stage primaryStage;
+
+    @Override
+    public void Start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+
+    }
+
+
 }

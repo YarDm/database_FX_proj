6ac51e0a46fd33c216736f7ed63405f013a7d43d
Ярослав <y.o.dmitriv@gmail.com>
2018-05-04 23:14:09 +0300

    Added a database and complete GUI

--- src/DbFx_main.java
+++ src/DbFx_main.java
@@ -2,6 +2,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /*
